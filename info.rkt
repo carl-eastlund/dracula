3ca2b23c93f6370082a42d363460b71aa7c8d618
Carl Eastlund <cce@racket-lang.org>
2014-02-24 03:28:08 -0500

    Updated info.rkt for planet package.

--- info.rkt
+++ info.rkt
@@ -1,19 +1,22 @@
 #lang setup/infotab
 
-;; Package:
-
-(define collection "dracula")
-(define deps '["planet-schematics-random1"])
-
 ;; setup-plt:
 
 (define name "Dracula")
 
-(define compile-omit-paths
-  (list
-    "acl2/gui.rkt"
-    "acl2/new-gui.rkt"
-    "acl2/program-controller.rkt"))
+;; Planet:
+(define blurb (list "Dracula: ACL2 theorem prover tools in DrRacket"))
+(define release-notes (list "8.24: updated to share code with package release"))
+(define categories '(devtools scientific))
+(define primary-file "main.rkt")
+(define repositories '("4.x"))
+(define required-core-version "5.92")
+(define homepage "http://www.ccs.neu.edu/home/cce/acl2/")
+
+;; Package:
+
+(define collection "dracula")
+(define deps '["planet-schematics-random1"])
 
 ;; tools:
 
